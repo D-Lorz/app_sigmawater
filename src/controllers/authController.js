4a97d8aeac91a931106c003564b24cfef217168b
Dorian L. Orozco <dalo1997@hotmail.es>
2022-04-18 23:52:38 -0500

    Corrección de inserción - Registro Final

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -23,20 +23,15 @@ exports.registrar = async (req, res) => {
         const numero_cuenta = req.body.numero_cuenta
         const ruta = req.body.ruta
         const beneficiario = req.body.beneficiario
-        // console.log("FRONTAL:>>>  ", req.nomArchivo[0]);
-        // console.log("TRASERA:>>>  ", req.nomArchivo[1]);
+        console.log("FRONTAL:>>>  ", req.nomArchivo[0]);
+        console.log("TRASERA:>>>  ", req.nomArchivo[1]);
 
-        // const frontal = '../imglicencias/sigmaWater_' + req.nomArchivo[0]
-        // const trasera = '../imglicencias/sigmaWater_' + req.nomArchivo[1]
-        // const licencia_conduccion = JSON.stringify({
-        //     'frontal': frontal,
-        //     'trasera': trasera
-        // });
-
-
-        //   console.log(nombres +"_"+apellidos+"_"+fecha_nacimiento+"_"+telefono_movil+"_"+correo+"_"+seguro_social+"_"+ciudad+"_"+direccion+"_"+apt_suite_unidad+"_"+
-        //   codigo_postal+""+codigo_referido+"_"+nombre_banco+"_"+numero_cuenta+"_"+ruta+"_"+beneficiario );
-        //   console.log("Licencia Front-Back >>> ", licencia_conduccion);
+        const frontal = '../imglicencias/sigmaWater_' + req.nomArchivo[0]
+        const trasera = '../imglicencias/sigmaWater_' + req.nomArchivo[1]
+        const licencia_conduccion = JSON.stringify({
+            'frontal': frontal,
+            'trasera': trasera
+        });
 
         const nuevoRegistro = {
             nombres,
@@ -54,8 +49,10 @@ exports.registrar = async (req, res) => {
             numero_cuenta,
             ruta,
             beneficiario,
-            // licencia_conduccion,
+            licencia_conduccion,
         }
+
+        console.log("DATOS A INSERTAR >>>", nuevoRegistro)
 
         await conexion.query('INSERT INTO formulario_registro_vendedor SET ?', [nuevoRegistro], (err, result) => {
             if (err) throw err;
@@ -64,125 +61,11 @@ exports.registrar = async (req, res) => {
             res.redirect('/login')
         })
 
-        //  conexion.query('INSERT INTO formulario_registro_vendedor SET ?', {
-        //     nombres:nombres,
-        //     apellidos: apellidos,
-        //     // fecha_nacimiento:fecha_nacimiento,
-        //     // telefono_movil: telefono_movil,
-        //     // correo:correo,
-        //     // seguro_social: seguro_social,
-        //     // ciudad:ciudad,
-        //     // direccion: direccion,
-        //     // apt_suite_unidad:apt_suite_unidad,
-        //     // codigo_postal: codigo_postal,
-        //     // codigo_referido:codigo_referido,
-        //     // nombre_banco:nombre_banco,
-        //     // numero_cuenta: numero_cuenta,
-        //     // ruta:ruta,
-        //     // beneficiario: beneficiario,
-        //     // licencia_conduccion:licencia_conduccion
-        // }, (error, results)=>{
-        //         if (error) {
-        //             res.json(error)
-        //             console.log(error);
-        //         } else {
-        //             // req.flash = ("success", "Tu registro ha sido recibido Muchas gracias!");
-        //             // res.redirect('/mensaje');
-        //             res.json(results)
-        //         }
-
-
-        //     });
-
-
     } catch (error) {
         conssole.log("Error de recepción de datos >>> ", error)
     }
-
 }
 
-
-//         const user = req.body.user
-//          conexion.query('INSERT INTO users SET ?', {user:user, name: name}, (error, results)=>{
-//             if(error){console.log(error)}
-//             res.redirect('/')
-//         })
-//     catch (error) {
-//         conssole.log(error)
-//     }       
-// 
-
-
-
-
-//TODO: REGISTER
-// exports.registrar = async (req, res) => {
-
-//     const nombres = req.body.nombres
-//     const apellidos = req.body.apellidosform
-//     const fecha_nacimiento = req.body.fechaform
-//     const numero_telefono = req.body.telefonoform
-//     const correo = req.body.correo
-//     const seguro_social = req.body.seguro
-//     const ciudad_donde_vive = req.body.ciudad
-//     const direccion = req.body.direccion
-//     const apt_suite_unidad = req.body.direccion2
-//     const codigo_postal = req.body.postal
-//     const numero_referido = req.body.referido
-//     const nombre_banco = req.body.banco
-//     const numero_de_cuenta = req.body.cuenta
-//     const numero_de_ruta = req.body.ruta
-//     const nombre_beneficiario = req.body.beneficiario
-
-// const frontal = '/imglicencias/sigmaWater_' + req.body.licencia
-// const trasera = '/imglicencias/sigmaWater_' + req.body.licencia_trasera
-
-// const licencia_conduccion = JSON.stringify({'frontal':frontal,'trasera':trasera});
-
-// console.log("FRONTAL:>>>  ", req.nomArchivo[0]);
-// console.log("TRASERA:>>>  ", req.nomArchivo[1]);
-
-// const frontal = '../imglicencias/sigmaWater_' + req.nomArchivo[0]
-// const trasera = '../imglicencias/sigmaWater_' + req.nomArchivo[1]
-
-// const licencia_conduccion = JSON.stringify({
-//     'frontal': frontal,
-//     'trasera': trasera
-// });
-
-// const nuevoRegistro = {
-//     nombres,
-//     apellidos,
-//     fecha_nacimiento,
-//     numero_telefono,
-//     correo,
-//     seguro_social,
-//     ciudad_donde_vive,
-//     direccion,
-//     apt_suite_unidad,
-//     codigo_postal,
-//     numero_referido,
-//     nombre_banco,
-//     numero_de_cuenta,
-//     numero_de_ruta,
-//     nombre_beneficiario,
-//     licencia_conduccion,
-// }
-
-// console.log("Licencia Front-Back >>> ", licencia_conduccion);
-// await conexion.query('INSERT INTO tblformulario_registro SET ?', [nuevoRegistro],
-
-//     (error, resultados) => {
-//         if (error) {
-//             res.json(error)
-//             console.log(error);
-//         } else {
-//             req.flash = ("success", "Tu registro ha sido recibido Muchas gracias!");
-//             res.redirect('/mensaje');
-//         }
-//     }
-// );
-// }
 //TODO: LOGIN
 exports.login = async (req, res) => {
     try {
