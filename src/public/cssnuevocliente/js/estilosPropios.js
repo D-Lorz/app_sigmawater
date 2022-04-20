ab7414da3a176701343a13587be5cdc077a45140
jcarlosabc <96692573+jcarlosabc@users.noreply.github.com>
2022-04-20 12:26:35 -0500

    validaciones + nuevos estilos + nuevos campos

---
+++ src/public/cssnuevocliente/js/estilosPropios.js
@@ -0,0 +1,30 @@
+const canvas = document.querySelector('canvas')
+const form = document.querySelector('.signature-pad-form')
+const clearButton = document.querySelector('.clear-button');
+const ctx = canvas.getContext('2d');
+
+let writingMode = false;
+
+form.addEventListener('submit', (event)=>{
+event.preventDefault();
+const imageURL = canvas.toDataURL();
+const image = document.createElement('img');
+image.src = imageURL;
+image.he
+
+})
+
+const clearPad = ()=>{
+
+}
+
+clearButton.addEventListener('click',(event)=>{
+
+
+})
+
+const getTargetPosition =(event) =>{
+
+
+
+}
