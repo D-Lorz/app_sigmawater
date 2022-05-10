92cd440e425c55059d2a29740e3f2b45d171f97b
jcarlosabc <96692573+jcarlosabc@users.noreply.github.com>
2022-05-10 17:07:04 -0500

    correcion de token + tiempo de expiracion

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -88,7 +88,7 @@ exports.login = async (req, res) => {
                 } else {
                  //inicio de sesión OK
                  const id = results[0].id
-                 const token = jwt.sign({ id: id },  'super_secret_AppSigmaWater', { expiresIn: 86400 })
+                 const token = jwt.sign({ id: id },  'super_secret_AppSigmaWater', { expiresIn: 3600 })
                  //generamos el token SIN fecha de expiracion
                  //const token = jwt.sign({id: id}, process.env.JWT_SECRETO)
                  console.log("TOKEN>>>>>: " + token + " para el USUARIO : " + correo)
@@ -129,7 +129,7 @@ exports.isAuthenticated = async (req, res, next) => {
             })
         } catch (error) {
             console.log(error)
-            return false
+            return next()
         }
     } else {
         res.redirect('/login')
