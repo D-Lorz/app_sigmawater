6872006af34174d3ad67eb84064665ac5524f1c8
Dorian L.O <dalo1997@hotmail.es>
2022-08-22 10:57:28 -0500

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,77 +4,6 @@ const conexion = require('../database/db')
 const { promisify } = require('util')
 const { log } = require('console')
 
-
-
-// todo: REGISTRAR
-exports.registrar = async (req, res) => {
-//  ? NOTA: ==>> Esta es la forma para obtener el año actual <<<<<
-   const year = new Date().getFullYear();
-//  ? NOTA: ==>> Esta es la forma para obtener el año actual <<<<<
-    let mes = new Date().getMonth()
-    mes == 0 ? mes = 12 : mes = mes+1
-//  ? NOTA: ==>> Esta es la forma para obtener el numero de la semana actual del año entero <<<<<
-   currentdate = new Date();
-   const oneJan = new Date(currentdate.getFullYear(),0,1);
-   const numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
-   const semana = Math.ceil(( currentdate.getDay() + numberOfDays ) / 7) - 1;
-   console.log("ESTA ES LA SEMANA ACTUAL ==>> " ,semana);
-//  ? NOTA: ==>> Esta es la forma para obtener la fecha actual <<<<<
-   const dia = new Date().getDate();
-
-    const nombres = req.body.nombres
-    const apellidos = req.body.apellidos
-    const fecha_nacimiento = req.body.fecha_nacimiento
-    const telefono_movil = req.body.telefono_movil
-    const correo = req.body.correo
-    const seguro_social = req.body.seguro_social
-    const ciudad = req.body.ciudad
-    const direccion = req.body.direccion
-    const apt_suite_unidad = req.body.apt_suite_unidad
-    const codigo_postal = req.body.codigo_postal
-    const codigo_afiliado = req.body.codigo_afiliado
-    const nombre_banco = req.body.nombre_banco
-    const numero_cuenta = req.body.numero_cuenta
-    const ruta = req.body.ruta
-    const beneficiario = req.body.beneficiario
-    const frontal = '../licences/' + urlLicencias[0]
-    const trasera = '../licences/' + urlLicencias[1]
-    const licencia_conduccion = JSON.stringify({ 'frontal': frontal, 'trasera': trasera});
-    const id_vendedor = generateRandomString(6)
-    const pass = generarPass_vendedor(8)
-  
-    const nuevoRegistro = {year,mes,semana,dia, nombres, apellidos, fecha_nacimiento, telefono_movil, 
-         correo, seguro_social, ciudad, direccion, apt_suite_unidad, codigo_postal, codigo_afiliado,
-         nombre_banco, numero_cuenta, ruta, beneficiario, licencia_conduccion,id_vendedor }
-    const usuarios = {correo, pass, id_vendedor, codigo_afiliado }
-
-     await conexion.query('INSERT INTO usuarios SET ?', [usuarios])
-     await conexion.query('INSERT INTO registro_de_vendedores SET ?', [nuevoRegistro])
-     let idConsecutivo = await conexion.query('SELECT id FROM registro_de_vendedores WHERE id_vendedor = ?', [id_vendedor])
-     
-     let fecha = new Date().toLocaleDateString("en-CA");
-     const numClientes = 00
-     const idVendedor = idConsecutivo[0].id
-     console.log("*************");
-     console.log("FECHA ==XXX", fecha);
-     console.log("numClientes ==XXX", numClientes);
-     console.log("idVendedor ==XXX", idVendedor);
-     console.log("*************");
-     const datos_PorDefectosCl = {fecha, numClientes, idVendedor }
-     console.log("DATOS XXXXX" , datos_PorDefectosCl);
-     await conexion.query('INSERT INTO historialnuevosclientes SET ?', [datos_PorDefectosCl])
-
-     let codeAfiliado = await conexion.query('SELECT codigo_afiliado, id_vendedor FROM registro_de_vendedores WHERE id_vendedor = ?', [id_vendedor])
-     let fechas = new Date().toLocaleDateString("en-CA");
-     const numAfiliados = 00
-     const datos_PorDefectosAfl = {fecha:fechas, numAfiliados:numAfiliados, idVendedor: codeAfiliado[0].id_vendedor }
-     console.log("DATOS XXXXX" , datos_PorDefectosAfl);
-
-   await conexion.query('INSERT INTO historialvendedores SET ?', [datos_PorDefectosAfl])
-   res.redirect('https://3csigmawater.com')
-}
-
-
 // todo: LOGIN
 exports.login = async (req, res) => {
     try {
@@ -202,7 +131,6 @@ exports.nologueado = async (req, res, next) => {
     if (!req.cookies.jwt) {
         return next()
     } else {
-
         res.redirect('/')
     }
 }
@@ -216,7 +144,8 @@ exports.isAdmin = async (req, res, next) => {
       console.log(error);
       return next();
     }
-  };
+};
+
 exports.isSeller = async (req, res, next) => {
     try {
       if (!(req.user.rol === "vendedor")) {
@@ -226,7 +155,9 @@ exports.isSeller = async (req, res, next) => {
       console.log(error);
       return next();
     }
-  };
+};
+
+
 
 // todo: MOSTRAR LISTA DE VENDEDORES AFILIADOS
 exports.listarAfiliados= async (req, res) => {
@@ -268,28 +199,3 @@ exports.facturacion = async (req, res) => {
 
     res.render('ventas-vendedor', { user: req.user, facturacionPropia, facturacionAfiliado })
 }
-
-
-// todo: GENERADOR DE CODIGO DE VENDEDOR APHANUMERICO
-const  generateRandomString = (num) => {
-    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let result1= '';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < num; i++ ) {
-        result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-
-    return result1;
-
-
-}
-// todo ===========>>> Generar codigo numero aleatorio del cliente
-const generarPass_vendedor = (num) => {
-    const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-    let result1 = "";
-    const charactersLength = characters.length;
-    for (let i = 0; i < num; i++) {
-      result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result1;
-  }
