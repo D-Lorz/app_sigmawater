7769a5941e5508bbba4c0f3427f3642297380acd
jcarlosabc <jeancatovio23@gmail.com>
2023-02-16 16:33:03 -0500

    Fix correo - estilos mobile del correo restablecer clave

--- src/lib/correo.js
+++ src/lib/correo.js
@@ -693,7 +693,95 @@ const aceptarVendedorHTML = (nomVendedor, clave) => {
 
 // Correo de recuperar contraseña
 const recuperarClaveHTML = (token) => {
-	return `<body style="background-color: transparent; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+	return `
+    <!DOCTYPE html>
+
+    <html lang="en" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
+    <head>
+    <title></title>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+    <!--[if !mso]><!-->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css"/>
+    <!--<![endif]-->
+        <style>
+            * {
+                box-sizing: border-box;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+            }
+
+            a[x-apple-data-detectors] {
+                color: inherit !important;
+                text-decoration: inherit !important;
+            }
+
+            #MessageViewBody a {
+                color: inherit;
+                text-decoration: none;
+            }
+
+            p {
+                line-height: inherit
+            }
+
+            .desktop_hide,
+            .desktop_hide table {
+                mso-hide: all;
+                display: none;
+                max-height: 0px;
+                overflow: hidden;
+            }
+
+            @media (max-width:620px) {
+
+                .desktop_hide table.icons-inner,
+                .social_block.desktop_hide .social-table {
+                    display: inline-block !important;
+                }
+
+                .icons-inner {
+                    text-align: center;
+                }
+
+                .icons-inner td {
+                    margin: 0 auto;
+                }
+
+                .row-content {
+                    width: 100% !important;
+                }
+
+                .mobile_hide {
+                    display: none;
+                }
+
+                .stack .column {
+                    width: 100%;
+                    display: block;
+                }
+
+                .mobile_hide {
+                    min-height: 0;
+                    max-height: 0;
+                    max-width: 0;
+                    overflow: hidden;
+                    font-size: 0px;
+                }
+
+                .desktop_hide,
+                .desktop_hide table {
+                    display: table !important;
+                    max-height: none !important;
+                }
+            }
+	    </style>
+    </head>
+    <body style="background-color: transparent; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
     <table border="0" cellpadding="0" cellspacing="0" class="nl-container" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: transparent;" width="100%">
     <tbody>
     <tr>
@@ -744,7 +832,6 @@ const recuperarClaveHTML = (token) => {
     <div style="text-decoration:none;display:block;color:#000000;background-color:#fed061;border-radius:6px;width:80%; width:calc(80% - 2px);border-top:1px solid #fed061;font-weight:400;border-right:1px solid #fed061;border-bottom:1px solid #fed061;border-left:1px solid #fed061;padding-top:5px;padding-bottom:5px;text-align:center;mso-border-alt:none;word-break:keep-all;"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:normal;"><span style="font-size: 16px; line-height: 2; word-break: break-word; mso-line-height-alt: 32px;">Restablecer</span></span></div>
     </a>
 
-
     <!--[if mso]></center></v:textbox></v:roundrect><![endif]-->
     </div>
     </td>
@@ -782,8 +869,8 @@ const recuperarClaveHTML = (token) => {
     </td>
     </tr>
     </tbody></table>
-    <div class="spacer_block mobile_hide" style="height:25px;line-height:25px;font-size:1px;">&hairsp;</div>
-    <div class="spacer_block" style="height:5px;line-height:5px;font-size:1px;">&hairsp;</div>
+    <div class="spacer_block mobile_hide" style="height:25px;line-height:25px;font-size:1px;"></div>
+    <div class="spacer_block" style="height:5px;line-height:5px;font-size:1px;"></div>
     </td>
     </tr>
     </tbody>
@@ -865,7 +952,7 @@ const recuperarClaveHTML = (token) => {
     <tbody>
     <tr>
     <td class="column column-1" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
-    <div class="spacer_block" style="height:30px;line-height:30px;font-size:1px;">&hairsp;</div>
+    <div class="spacer_block" style="height:30px;line-height:30px;font-size:1px;"></div>
     </td>
     </tr>
     </tbody>
