edecfebaf4c93f4bea984dc20b292ab4e9285d90
jcarlosabc <jeancatovio23@gmail.com>
2023-02-20 17:40:21 -0500

    Add variable global para btn copiar codigo

--- src/app.js
+++ src/app.js
@@ -47,6 +47,8 @@ global.urlEvidenciaServicioInstalado = ['front'];
 global.urlProfile = ''
 global.urlEvidencia = ''
 
+global.btn_mydomain = 'http://localhost:' + port
+
 // No almacenar caché
 app.use((req, res, next) => {
   res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
