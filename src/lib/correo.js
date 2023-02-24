475bb99b5d982dcbfd9a58aea5bc36aa430c62fc
jcarlosabc <jeancatovio23@gmail.com>
2023-02-24 10:07:58 -0500

    Fix config transport rejectUnauthorized

--- src/lib/correo.js
+++ src/lib/correo.js
@@ -25,12 +25,25 @@ const mail = {
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-    service: "Godaddy",
+    host: "smtp.office365.com", // hostname
+    port: 587,
+    secure: false, // TLS requires secureConnection to be false
 	auth: {
 		user: mail.user, // generated ethereal user
 		pass: mail.pass, // generated ethereal password
 	},
+    tls:{
+        rejectUnauthorized: false
+   }
 });
+
+transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
 
 
 /** Enviar el email */
